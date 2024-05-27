5525c5f3190ecab19eb78ac771197042b710ddbb
iQily <rifqi.aly@gmail.com>
2024-05-27 22:51:13 +0700

    hero page, DONE

---
+++ components/Hero.tsx
@@ -0,0 +1,44 @@
+'use client';
+import React from 'react'
+import Image from "next/image";
+import animationData from '../public/lotties/world-transfer.json'
+
+import CustomButton from './CustomButton'
+
+const Hero = () => {
+   
+
+    const handleScroll = () => {
+
+    }
+    return (
+        <div className='hero'>
+            <div className='flex-1 pt-36 padding-x'>
+                <h1 className='hero__title'>Find, buy, and get the Article — quickly and easily!</h1>
+                <p className='hero__subtitle'>
+                    Streamline your buying article experience with our effortless process.
+                </p>
+                <CustomButton
+                    title='Explore Articles'
+                    containerStyles='bg-primary-blue
+                    text-white rounded-full mt-10'
+                    handleClick={handleScroll}
+                />
+            </div>
+
+            <div className='hero__image-container'>
+                <div className="hero__image">
+                    <Image
+                        src='/hero.png'
+                        alt='hero'
+                        fill
+                        className="object-contain"
+                    />
+                    <div className='hero__image-overlay'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
